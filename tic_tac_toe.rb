810acb8fec9ce6884e247c9661e4a206034d6171
overlord88 <yuyueugene84@gmail.com>
2014-11-19 18:43:30 +0800

    refactored

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,6 +9,7 @@ require 'pry'
 
 def print_grid(arr)
 
+system 'clear'
 puts "#{arr[0]}|#{arr[1]}|#{arr[2]}"
 puts "-----"
 puts "#{arr[3]}|#{arr[4]}|#{arr[5]}"
@@ -16,6 +17,7 @@ puts "-----"
 puts "#{arr[6]}|#{arr[7]}|#{arr[8]}"
 puts "-----"
 puts "     "
+
 end
 
 def initialize_array(arr)
@@ -25,7 +27,6 @@ def initialize_array(arr)
 end
 
 def check_win(arr)
-  #win_conditions = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
   win_conditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [3,5,8], [0,4,8], [2,4,6]]
   win_conditions.each do |condition|
     return 1 if arr.values_at(*condition).count('X') == 3
@@ -40,49 +41,44 @@ def check_full(arr)
   end
 end
 
+def check_filled(board, position)
+  if board[position.to_i - 1] != " "
+    true
+  else
+    false
+  end
+end
+
 puts "Lets play Tic Tac Toe!"
 playing = true
-arr = []
-initialize_array(arr)
-print_grid(arr)
+board = []
+initialize_array(board)
+print_grid(board)
 
 begin
-  puts "Choose a position (from 1 to 9 ) to place a piece"
-  user_position = gets.chomp
 
-  if !(1..9).to_a.include?(user_position.to_i) #check if user has entered a valid input
-    begin
-      puts "Please choose a valid position, from 1 - 9:"
-      user_position = gets.chomp
-    end while ![1..9].include?(user_position.to_i)
-  end
+  begin
+    puts "Please choose a position, from 1 - 9:"
+    user_position = gets.chomp
+  end while !(1..9).to_a.include?(user_position.to_i)
 
-  if arr[user_position.to_i - 1] != " " #check if user input grid is already occupied
+  if check_filled(board, user_position) == true #check if user input grid is already occupied
     begin
       puts "Grid already filled, please choose another position, from 1 - 9:"
       user_position = gets.chomp
-    end while arr[user_position.to_i - 1] != " "
+    end while check_filled(board, user_position) == true
   end
 
-  arr[user_position.to_i - 1] = "X" #user input will print X in grid
+  board[user_position.to_i - 1] = "X" #user input will print X in grid
+  print_grid(board)
 
-  print_grid(arr)
+  begin #if computer position is already occupied, keep generate until a valid input pops out
+    computer_position = rand(1..9)
+  end while check_filled(board, computer_position) == true
 
-  computer_position = rand(1..9) #generate a random number
-
-  if arr[computer_position - 1] != " " #if computer position is already occupied, keep generate until a valid input pops out
-    begin
-      computer_position = rand(1..9)
-    end while arr[computer_position - 1] != " "
-  end
-
-  arr[computer_position.to_i - 1] = "O" #computer input will print O in grid
-
-  puts "Now it's Computer's turn:"
-
-  print_grid(arr)
-
-  result = check_win(arr)
+  board[computer_position.to_i - 1] = "O" #computer input will print O in grid
+  print_grid(board)
+  result = check_win(board)
 
   if result != nil
     if result == 1
@@ -94,5 +90,5 @@ begin
     end
   end
 
-  break if playing == false || check_full(arr) == false
-end while playing == true || check_full(arr)== true
+  break if playing == false || check_full(board) == false
+end while playing == true || check_full(board)== true
